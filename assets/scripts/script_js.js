4ccc2c76352d844b39a4f4a345a2f6fb4397ba29
adrian sanchez carchano <adriansanchezcarchano@gmail.com>
2018-02-22 21:35:26 +0100

    destroy the project now

--- assets/scripts/script_js.js
+++ assets/scripts/script_js.js
@@ -1,29 +1,23 @@
 window.onload = function(e) {
-	init();
 
-/*
+	document.getElementsByClassName("w3-display-left").onclick=plusDivs(-1);
+
+	var slideIndex = 1;
 	showDivs(slideIndex);
 
 	function plusDivs(n) {
-	  showDivs(slideIndex += n);
+		showDivs(slideIndex += n);
 	}
 
 	function showDivs(n) {
-	  var i;
-	  var x = document.getElementsByClassName("mySlides");
-	  if (n > x.length) {slideIndex = 1}    
-	  if (n < 1) {slideIndex = x.length}
-	  for (i = 0; i < x.length; i++) {
-	     x[i].style.display = "none";  
-	  }
-	  x[slideIndex-1].style.display = "block";  
-	}	
-*/
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n > x.length) {slideIndex = 1} 
+	if (n < 1) {slideIndex = x.length} ;
+	for (i = 0; i < x.length; i++) {
+	    x[i].style.display = "none"; 
+	}
+	x[slideIndex-1].style.display = "block"; 
+	}
+	
 }
-
-function init(){
-	var slideIndex = 1;
-	console.log("Hemos crea slider "+slideIndex);
-}
-
-
